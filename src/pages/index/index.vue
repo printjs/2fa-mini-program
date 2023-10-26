694bba7f03354070baf7ec8554606e5b871dac9a
Jiantao Wu （吴建涛） <jiantao.wu@cerence.com>
2023-10-26 16:59:30 +0800

    inital program

---
+++ src/pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <view class="content">
+    <button
+      type="primary"
+      size="mini"
+      hover-class="button-hover"
+      @click="start"
+    >
+      开始
+    </button>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('Hello Jiantao')
+
+function start() {
+  uni.scanCode({
+    success (res) {
+      console.log(res)
+    },
+    fail(data) {
+      console.log(data)
+    }
+  })
+
+}
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
